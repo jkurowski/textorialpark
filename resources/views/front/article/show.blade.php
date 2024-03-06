d79f3d6cac12730f9e3814bfe92751ef2e0cb709
Jacek <kontakt@jk-design.pl>
2024-03-07 00:27:57 +0100

    Formularz kontaktowy w stopce

--- resources/views/front/article/show.blade.php
+++ resources/views/front/article/show.blade.php
@@ -10,24 +10,28 @@
 @stop
 
 @section('content')
-    <section class="container">
-        <div class="row d-flex justify-content-center">
-            <div class="col-12">
-                <div class="post-details">
-                    <picture>
-                        <source type="image/webp" srcset="{{asset('uploads/articles/webp/'.$article->file_webp) }}">
-                        <source type="image/jpeg" srcset="{{asset('uploads/articles/'.$article->file) }}">
-                        <img src="{{asset('uploads/articles/'.$article->file) }}" alt="{{ $article->title }}" class="w-100">
-                    </picture>
+    <section class="container container-dashed">
+        <div class="row justify-content-center">
+            <div class="col-10">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="post-details">
+                            <picture>
+                                <source type="image/webp" srcset="{{asset('uploads/articles/webp/'.$article->file_webp) }}">
+                                <source type="image/jpeg" srcset="{{asset('uploads/articles/'.$article->file) }}">
+                                <img src="{{asset('uploads/articles/'.$article->file) }}" alt="{{ $article->title }}" class="w-100">
+                            </picture>
 
-                    <div class="post-details-entry mt-5 mb-3">
-                        <h1 class="post-details-title mb-5">{{ $article->title }}</a></h1>
-                        <h3>{{$article->content_entry}}</h3>
+                            <div class="post-details-entry mt-5 mb-3">
+                                <h1 class="post-details-title mb-5">{{ $article->title }}</a></h1>
+                                <h3>{{$article->content_entry}}</h3>
+                            </div>
+                            <div class="post-details-text">
+                                <p>{!! parse_text($article->content) !!}</p>
+                            </div>
+                            <a href="{{route('front.news.index')}}" class="bttn mt-3 mt-md-5">@lang('website.button-back-to-list')</a>
+                        </div>
                     </div>
-                    <div class="post-details-text">
-                        <p>{!! parse_text($article->content) !!}</p>
-                    </div>
-                    <a href="{{route('front.news.index')}}" class="bttn mt-3 mt-md-5">@lang('website.button-back-to-list')</a>
                 </div>
             </div>
         </div>
